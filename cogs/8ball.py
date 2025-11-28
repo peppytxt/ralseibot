b7ad5d71fa677ed24a70a88d7f3340d246b52164
Pedro <pedrocn236@gmail.com>
2025-11-28 18:24:38 -0400

    bola 8 correção de erro

--- cogs/8ball.py
+++ cogs/8ball.py
@@ -9,7 +9,7 @@ class ball8(commands.Cog):
         self.bot = bot
     
     @app_commands.command(name="8ball", description="Um oráculo")
-    async def bola8(interaction: discord.Interaction, message:str):
+    async def bola8(self, interaction: discord.Interaction, message: str):
         msg = ["Sim", "Não", "Talvez", "Provavelmente sim", "Provavelmente não", "Tente novamente...", "Quem sabe...", "Fake", "Verídico", "Sou inteligência artificial e não bola de cristal, bobão.", "Com certeza não", "Trouxa", "Não quero nem saber", "Não sei e nem quero saber", "Me dá robux que te conto", "Por você sim 👉👈😳", "fds", "Pelo meus cálculos sim", "Pelos meus cálculos não", "Preciso mesmo responder isso?", "Detergente", "Óbvio", "Nunca", "Jamais"]
         msgrandom = random.choice(msg)
         await interaction.response.send_message(f"{interaction.user}: {message}\n🎱 **{msgrandom}**")
