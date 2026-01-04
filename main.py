27f1eabfaec1082ba308212eee5fbf583017a006
Pedro <pedrocn236@gmail.com>
2026-01-04 15:04:08 -0400

    autorizando main para aceitar xp voice

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ MONGO_URL = os.getenv("MONGO_URL")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.voice_states = True
 intents.members = True
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
